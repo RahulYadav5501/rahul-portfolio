5191c57a710acb35c59afe19f36e555637b0da89
Rahul Yadav <rahulyadav04062003@gmail.comgit config --global user.email rahulyadav04062003@gmail.com>
2025-05-14 17:24:25 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:  "/rahul-portfolio",
+  base: process.env.VITE_BASE_PATH || "/rahul-portfolio",
 
 });
